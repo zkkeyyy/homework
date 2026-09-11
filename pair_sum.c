046e99bf775fde0c7e0150da8d45ee68c6245d0c
zkkeyyy <zkey07@qq.com>
2026-09-11 11:52:50 +0800

    作业

---
+++ pair_sum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int price[6] = {18, 7, 25, 12, 33, 20};
+	int n = 6;
+	int target = 32;
+	int checks = 0;
+	
+	for (int i = 0; i < n; ++i) {
+		for (int j = i + 1; j < n; ++j) {
+			++checks;
+			
+			if (price[i] + price[j] == target) {
+				printf("indices: %d, %d\n", i, j);
+				printf("checks: %d\n", checks);
+				return 0;
+			}
+		}
+	}
+	
+	printf("no pair\n");
+	printf("checks: %d\n", checks);
+	
+	return 0;
+}
